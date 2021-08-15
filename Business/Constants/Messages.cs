b1372e7587477fdccac8ad8396ee00f09b5f514a
Togrul Masimli <togrul.mesimli02@gmail.com>
2021-08-15 21:22:43 +0400

    Entites Logic Added

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,15 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Product Added";
+        public static string ProductNameInvalid = "Product Name Is Invalid";
+        public static string MaintenanceTime = "Maintenance Time";
+        public static string ProductsListed = "Products Listed";
+    }
+}
